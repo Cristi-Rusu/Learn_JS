e2a2266c88fc65c8da24a52d3d183b9e440fd180
Cristi Rusu <cristi.rusu.v@gmail.com>
2019-05-19 16:54:03 +0300

    practice classes, prototype methods, the iterator interface and Symbols

--- js/prototypes/chapter.js
+++ js/prototypes/chapter.js
@@ -24,7 +24,7 @@ SimpleRabbit.prototype.eat = function( food ) {
 };
 
 let whiteRabbit = new SimpleRabbit('White');
-console.log(whiteRabbit);
+// console.log(whiteRabbit);
 
 // 'class' notation in JavaScript
 // the function named 'constructor' inside a class is treated as the constructor function
@@ -41,5 +41,140 @@ class Rabbit {
     }
 }
 
+let blackRabbit = new Rabbit('Black');
 let happyRabbit = new Rabbit('Happy');
-console.log(happyRabbit);
+let killerRabbit = new Rabbit('Killer');
+// console.log(happyRabbit);
+
+// prototype properties are overwritten by the objects properties
+Rabbit.prototype.teeth = 'small';
+
+// console.log('killerRabbit teeth:', killerRabbit.teeth);
+// -> 'small'
+
+killerRabbit.teeth = 'long and sharp';
+// console.log('killerRabbit teeth updated:', killerRabbit.teeth);
+// -> 'long and sharp'
+// console.log('blackRabbit teeth:', blackRabbit.teeth);
+// -> 'small'
+
+// Maps in js are use to store keys and their values
+let ages = new Map();
+// to add a key and a value use the method 'set'
+ages.set('Boris', 39);
+ages.set('Jack', 67);
+ages.set('Julia', 23);
+
+// use 'get' to use a value of a key
+// console.log(`Boris' age is ${ages.get('Boris')}`);
+// to find if a key exists in a map, use the method 'has'
+// console.log(`Is Vlad's age known? ${ages.has('Vlad')}`);
+
+// 'hasOwnProperty' can be used on an object to find if it contains a specific key itself, not in the prototype
+// console.log({x: 1}.hasOwnProperty('x'));
+// console.log({x: 1}.hasOwnProperty('toString'));
+
+// methods of an object can be overwritten to be able to call them with functions like 'String'
+// which look for the 'toString' property in the prototype and use it to return a more meaningful result than [object Object]
+// this concept is called Polymorphism
+Rabbit.prototype.toString = function() {
+    return `a ${this.type} rabbit`;
+};
+
+// console.log(String(killerRabbit));
+
+// Symbols are suitable for defining interfaces
+// they are created with the function 'Symbol' and are unique
+let sym = Symbol('sym');
+// console.log(sym === Symbol('sym'));
+
+// thank's to symbols, properties with the same name can be inside an interface
+const toStringSym = Symbol('toStringSym');
+Array.prototype[toStringSym] = function() {
+    return `An array with ${this.length} items`;
+};
+// to access the symbol, use the square brackets notation
+// console.log([1,2,3].toString());
+// -> 1,2,3
+// console.log([1,2,3][toStringSym]());
+// -> An array with 3 items
+
+// it is possible to include symbol properties in objects by simply using the square brackets
+let stringObject = {
+    [toStringSym]() {
+        return `a jute rope`;
+    }
+};
+// console.log(stringObject[toStringSym]());
+
+
+class Matrix {
+    constructor( width, height, element = (x, y) => undefined ) {
+        this.width = width;
+        this.height = height;
+        this.content = [];
+
+        // create the matrix
+        for ( let y = 0; y < height; y++ ) {
+            for ( let x = 0; x < width; x++ ) {
+                // this matrix is a flat array, so to traverse the matrix
+                // we use the formula "y * width + x"
+                this.content[y * width + x] = element(x, y);
+            }
+        }
+    }
+    // retrieve an element
+    get( x, y ) {
+        return this.content[y * this.width + x];
+    }
+    // update an element in the matrix
+    set( x, y, value ) {
+        this.content[y * this.width + x] = value;
+    }
+    // make the 'Matrix' iterable directly in the class
+    [Symbol.iterator]() {
+        return new MatrixIterator(this);
+    }
+}
+
+class MatrixIterator {
+    constructor( matrix ) {
+        this.x = 0;
+        this.y = 0;
+        this.matrix = matrix;
+    }
+
+    next() {
+        // if 'y' is equal to the matrix height, the loop is done
+        if ( this.y === this.matrix.height ) return {done: true};
+
+        let value = {
+            x: this.x,
+            y: this.y,
+            value: this.matrix.get(this.x, this.y)
+        };
+        
+        this.x++;
+        // if 'x' is equal to the matrix width, move to the next row
+        if ( this.x === this.matrix.width ) {
+            this.x = 0;
+            this.y++;
+        }
+
+        return {value, done: false};
+    }
+}
+// make the 'Matrix' prototype iterable by changing the prototype
+// Matrix.prototype[Symbol.iterator] = function() {
+//     return new MatrixIterator(this);
+// };
+
+let matrix = new Matrix(3, 2, (x, y) => `(${x}, ${y})` );
+// the 'Matrix' prototype is iterable
+for (let {x, y, value} of matrix) {
+    matrix.set(x, y, `${x * 3}, ${y * 3}`);
+}
+console.log(matrix);
+
+let matrix2 = new Matrix(3, 3, (x, y) => `(${x}, ${y})` );
+console.log(matrix2);
